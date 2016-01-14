0ae4ea2cca58bd2aa3ab68ee5dae47ed8c9fcda1
Jean-Denis Kreiss <jean-denis.kreiss@agixis.com>
2016-01-14 11:12:49 +0100

    Create .net 4 support files

---
+++ Mapsui/ExtensionMethods/Net4Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Mapsui
+{
+    public static class Net4Extensions
+    {
+        public static void Dispose(this WebResponse response)
+        {
+            response.Close();
+        }
+    }
+}
